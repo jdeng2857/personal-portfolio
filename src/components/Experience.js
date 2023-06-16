f684c0d1d39e2f6d0e6af0a98784f7c503eaba8b
jdeng2857 <jdeng2857@gmail.com>
2023-06-16 19:29:04 -0400

    Add base experience section with images

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,11 +1,12 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
-import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
-import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+
+import kubernetesImg from '../assets/img/technologies/kubernetes.png';
+import gcpImg from '../assets/img/technologies/gcp.png';
+import terraformImg from '../assets/img/technologies/terraform.png';
+import nginxImg from '../assets/img/technologies/nginx.png';
+import golangImg from '../assets/img/technologies/golang.png';
 
 export const Experience = () => {
 
@@ -19,7 +20,7 @@ export const Experience = () => {
   ];
 
   return (
-    <section className="project" id="project">
+    <section className="experience" id="experience">
       <Container>
         <Row>
           <Col size={12}>
@@ -27,14 +28,24 @@ export const Experience = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Experience</h2>
-                <p>Professional work experience in cloud infrastructure, backend, and frontend development.</p>
-        
+                <h3>Shopify | Infrastructure Engineer Intern</h3>
+                <p>Developed HTTPS L7 routing infrastructure with Google Kubernetes Engine, involving updating custom NGINX
+middleware written in Lua, provisioning SSL certificates and firewall rules, traffic management, handling 20 million RPM</p>
+                <p>Built observability tooling with Prometheus metrics and integrated request latency dashboards with alerts</p>
+                <p>Implemented automated memory deletion for Kubernetes controller in Golang, reducing resources used</p>
+                <p>Upgraded Kubernetes cluster fleet in Terraform with custom developer tools, improving infrastructure security</p>
+                <p>Reduced latency between two major services by 75% using Kubernetes load testing</p>
+                <p>Skills: Kubernetes, Google Cloud Platform (GCP), Terraform, NGINX, Golang</p>
+                <img className="kubernetes" src={kubernetesImg}></img>
+                <img className="gcp" src={gcpImg}></img>
+                <img className="terraform" src={terraformImg}></img>
+                <img className="nginx" src={nginxImg}></img>
+                <img className="golang" src={golangImg}></img>
               </div>} 
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
     </section>
   )
 }
